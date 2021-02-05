b3c26ee902b8d01c8bd9126eec0291e9471d3edb
jia-ern <67193913+jia-ern@users.noreply.github.com>
2021-02-06 00:33:00 +0800

    Update BuyerPresentationEdit.aspx.cs

--- ArtMoments/Sites/usermgmt/BuyerPresentationEdit.aspx.cs
+++ ArtMoments/Sites/usermgmt/BuyerPresentationEdit.aspx.cs
@@ -6,16 +6,17 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Data;
+using System.Configuration;
+using System.IO;
 
 namespace ArtMoments.Sites.usermgmt
 {
     public partial class BuyerPresentationEdit : System.Web.UI.Page
     {
         string connectionString = "Data Source=LAPTOP-RF7VE486\\SQLEXPRESSFJE;Initial Catalog=ArtMomentsDb; Integrated Security=True; User ID=sa;Password=***********";
-
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
+            if (!this.IsPostBack)
             {
                 if (Session["UserName"] == null)
                 {
@@ -24,37 +25,17 @@ namespace ArtMoments.Sites.usermgmt
 
                 String tbUserName = Session["UserName"].ToString();
 
-                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                using (SqlConnection conn = new SqlConnection(connectionString))
                 {
-                    sqlCon.Open();
-                    //@UserID, @UserName, @UserPassword, @UserConfirmedPassword, @UserEmail, @Bibliography, @UserContactNo, @UserAddress
                     String query = "SELECT * FROM [User] WHERE user_name = @UserName";
-                    SqlCommand cmd = new SqlCommand(query, sqlCon);
 
-                    cmd.Parameters.AddWithValue("@UserName", tbUserName);
-                    SqlDataReader dr = cmd.ExecuteReader();
-
-                    if (dr.HasRows)
+                    using (SqlDataAdapter sda = new SqlDataAdapter(query, conn))
                     {
-                        while (dr.Read())
-                        {
-                            tbBibliography.Text = dr.GetValue(5).ToString();
-                            if (tbBibliography.Text == "")
-                            {
-                                tbBibliography.Text = "Enter your bibliography to let others know you better";
-                            }
-
-                            tbProfilePic.Text = dr.GetValue(6).ToString();
-
-                            if (tbProfilePic.Text == "")
-                            {
-                                tbProfilePic.Text = "Click edit to insert your image url";
-                            }
-                            else
-                            {
-                                Image1.ImageUrl = tbProfilePic.Text;
-                            }
-                        }
+                        //sda.SelectCommand.Parameters.AddWithValue("@UserName", "%" + tbUserName + "%");
+                        //DataTable dt = new DataTable();
+                        //sda.Fill(dt);
+                        //gvImages.DataSource = dt;
+                        //gvImages.DataBind();
                     }
                 }
             }
@@ -75,37 +56,59 @@ namespace ArtMoments.Sites.usermgmt
             Response.Redirect("BuyerPresentationEdit.aspx");
         }
 
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            using (SqlConnection sqlCon = new SqlConnection(connectionString))
-            {
-                sqlCon.Open();
-
-                String tbUserName = Session["UserName"].ToString();
-                String query1 = "UPDATE [User] SET [bibliography] = @Bibliography, [profile_pic] = @ProfilePic WHERE user_name = @UserName";
-
-                SqlCommand sqlCmd = new SqlCommand(query1, sqlCon);
-
-                sqlCmd.Parameters.AddWithValue("@UserName", tbUserName);
-                sqlCmd.Parameters.AddWithValue("@Bibliography", tbBibliography.Text);
-                sqlCmd.Parameters.AddWithValue("@ProfilePic", tbProfilePic.Text);
-
-                sqlCmd.ExecuteNonQuery();
-                lblSuccessMessage.Text = "Edited Successfully";
-                //RedirectAfterDelayFn();
-                sqlCon.Close();
-                Response.Redirect("BuyerPresentation.aspx");
-            }
-        }
-
         protected void tbBibliography_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        protected void tbProfilePic_TextChanged1(object sender, EventArgs e)
+        protected void OnRowDataBound(object sender, GridViewRowEventArgs e)
         {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                DataRowView dr = (DataRowView)e.Row.DataItem;
+                string imageUrl = "data:image/jpg;base64," + Convert.ToBase64String((byte[])dr["profile_pic"]);
+                (e.Row.FindControl("Image1") as Image).ImageUrl = imageUrl;
 
+
+
+                
+            }
+        }
+
+        protected void Upload(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                String tbUserName = Session["UserName"].ToString();
+                byte[] bytes;
+                using (BinaryReader br = new BinaryReader(FileUpload1.PostedFile.InputStream))
+                {
+                    bytes = br.ReadBytes(FileUpload1.PostedFile.ContentLength);
+                }
+
+
+                //< add name = "constr" connectionString = "Data Source=.\SQL2014;Initial Catalog=dbFiles;Integrated Security=true" />
+                //string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+
+                    string sql = "UPDATE [User] SET user_contactno = @Name, [bibliography] = @ContentType, [profile_pic] = @ProfilePic WHERE user_name = @UserName";
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@UserName", tbUserName);
+                        cmd.Parameters.AddWithValue("@Name", Path.GetFileName(FileUpload1.PostedFile.FileName));
+                        cmd.Parameters.AddWithValue("@ContentType", FileUpload1.PostedFile.ContentType);
+                        cmd.Parameters.AddWithValue("@Bibliography", tbBibliography.Text);
+                        cmd.Parameters.AddWithValue("@ProfilePic", bytes);
+
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        conn.Close();
+                    }
+                }
+
+                Response.Redirect(Request.Url.AbsoluteUri);
+            }
         }
     }
 }
