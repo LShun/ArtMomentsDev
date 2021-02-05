7aa2a2ba091c212b4691fb8d6f20412b7df8d4e0
HueyPeng17 <59443995+HueyPeng17@users.noreply.github.com>
2021-02-05 18:53:17 +0800

    Create EditProduct.aspx.cs

---
+++ ArtMoments/Sites/artist/EditProduct.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ArtMoments.Sites.artist
+{
+    public partial class EditProduct : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void saveProdBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void resetProdBtn_Click(object sender, EventArgs e)
+        {
+            ClearInputs(Page.Controls);
+        }
+        void ClearInputs(ControlCollection ctrls)
+        {
+            foreach (Control ctrl in ctrls)
+            {
+                if (ctrl is TextBox)
+                    ((TextBox)ctrl).Text = string.Empty;
+                ClearInputs(ctrl.Controls);
+            }
+        }
+    }
+}
