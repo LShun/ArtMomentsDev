b79ad720443e997eef0355d39a8d669db4043bf5
jia-ern <67193913+jia-ern@users.noreply.github.com>
2021-02-06 13:42:56 +0800

    Update BuyerPresentationEdit.aspx.cs

--- ArtMoments/Sites/usermgmt/BuyerPresentationEdit.aspx.cs
+++ ArtMoments/Sites/usermgmt/BuyerPresentationEdit.aspx.cs
@@ -4,16 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Data.SqlClient;
+
+using System.IO;
 using System.Data;
 using System.Configuration;
-using System.IO;
+using System.Data.SqlClient;
+
 
 namespace ArtMoments.Sites.usermgmt
 {
+
     public partial class BuyerPresentationEdit : System.Web.UI.Page
     {
+        //String connectionString = @"Data Source =(local)\SQLEXPRESSFJE; initial Catalog=tblFile; Integrated Security = True;";
         string connectionString = "Data Source=LAPTOP-RF7VE486\\SQLEXPRESSFJE;Initial Catalog=ArtMomentsDb; Integrated Security=True; User ID=sa;Password=***********";
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!this.IsPostBack)
@@ -22,43 +28,22 @@ namespace ArtMoments.Sites.usermgmt
                 {
                     Response.Redirect("PreLogin.aspx");
                 }
-
-                String tbUserName = Session["UserName"].ToString();
-
+                //string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
                 using (SqlConnection conn = new SqlConnection(connectionString))
                 {
-                    String query = "SELECT * FROM [User] WHERE user_name = @UserName";
+                    string UserName = Session["UserName"].ToString();
 
-                    using (SqlDataAdapter sda = new SqlDataAdapter(query, conn))
+                    using (SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM [User] WHERE user_name = @UserName", conn))
                     {
-                        //sda.SelectCommand.Parameters.AddWithValue("@UserName", "%" + tbUserName + "%");
-                        //DataTable dt = new DataTable();
-                        //sda.Fill(dt);
-                        //gvImages.DataSource = dt;
-                        //gvImages.DataBind();
+                        //sqlDa.SelectCommand.Parameters.AddWithValue("@UserID", userID);
+                        sda.SelectCommand.Parameters.AddWithValue("@UserName", UserName);
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+                        gvImages.DataSource = dt;
+                        gvImages.DataBind();
                     }
                 }
             }
-        }
-
-        protected void btnHome_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("BuyerAccount.aspx");
-        }
-
-        protected void btnSetting_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("BuyerSetting.aspx");
-        }
-
-        protected void btnPresentation_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("BuyerPresentationEdit.aspx");
-        }
-
-        protected void tbBibliography_TextChanged(object sender, EventArgs e)
-        {
-
         }
 
         protected void OnRowDataBound(object sender, GridViewRowEventArgs e)
@@ -68,18 +53,13 @@ namespace ArtMoments.Sites.usermgmt
                 DataRowView dr = (DataRowView)e.Row.DataItem;
                 string imageUrl = "data:image/jpg;base64," + Convert.ToBase64String((byte[])dr["profile_pic"]);
                 (e.Row.FindControl("Image1") as Image).ImageUrl = imageUrl;
-
-
-
-                
             }
         }
 
         protected void Upload(object sender, EventArgs e)
         {
-            if (!this.IsPostBack)
+            if (FileUpload1.PostedFile != null)
             {
-                String tbUserName = Session["UserName"].ToString();
                 byte[] bytes;
                 using (BinaryReader br = new BinaryReader(FileUpload1.PostedFile.InputStream))
                 {
@@ -87,20 +67,16 @@ namespace ArtMoments.Sites.usermgmt
                 }
 
 
-                //< add name = "constr" connectionString = "Data Source=.\SQL2014;Initial Catalog=dbFiles;Integrated Security=true" />
-                //string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
                 using (SqlConnection conn = new SqlConnection(connectionString))
                 {
-
-                    string sql = "UPDATE [User] SET user_contactno = @Name, [bibliography] = @ContentType, [profile_pic] = @ProfilePic WHERE user_name = @UserName";
+                    string UserName = Session["UserName"].ToString();
+                    string sql = "UPDATE [User] SET profile_pic = @Data WHERE user_name = @UserName";
                     using (SqlCommand cmd = new SqlCommand(sql, conn))
                     {
-                        cmd.Parameters.AddWithValue("@UserName", tbUserName);
-                        cmd.Parameters.AddWithValue("@Name", Path.GetFileName(FileUpload1.PostedFile.FileName));
-                        cmd.Parameters.AddWithValue("@ContentType", FileUpload1.PostedFile.ContentType);
-                        cmd.Parameters.AddWithValue("@Bibliography", tbBibliography.Text);
-                        cmd.Parameters.AddWithValue("@ProfilePic", bytes);
-
+                        cmd.Parameters.AddWithValue("@UserName", UserName);
+                        //cmd.Parameters.AddWithValue("@Name", Path.GetFileName(FileUpload1.PostedFile.FileName));
+                        //cmd.Parameters.AddWithValue("@ContentType", FileUpload1.PostedFile.ContentType);
+                        cmd.Parameters.AddWithValue("@Data", bytes);
                         conn.Open();
                         cmd.ExecuteNonQuery();
                         conn.Close();
@@ -108,7 +84,13 @@ namespace ArtMoments.Sites.usermgmt
                 }
 
                 Response.Redirect(Request.Url.AbsoluteUri);
+                
             }
+        }
+
+        protected void tbBibliography_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
