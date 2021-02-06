64289c6c8b72dc068dd46e467b74c7882e7d4b97
jia-ern <67193913+jia-ern@users.noreply.github.com>
2021-02-06 13:42:53 +0800

    Update BuyerPresentationEdit.aspx.designer.cs

--- ArtMoments/Sites/usermgmt/BuyerPresentationEdit.aspx.designer.cs
+++ ArtMoments/Sites/usermgmt/BuyerPresentationEdit.aspx.designer.cs
@@ -42,15 +42,6 @@ namespace ArtMoments.Sites.usermgmt
         protected global::System.Web.UI.WebControls.GridView gvImages;
 
         /// <summary>
-        /// SqlDataSource1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
-
-        /// <summary>
         /// btnUpload control.
         /// </summary>
         /// <remarks>
