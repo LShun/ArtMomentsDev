77d3badd3538279bd42094a8a128aa0bb11fb0e3
HueyPeng17 <59443995+HueyPeng17@users.noreply.github.com>
2021-02-10 23:49:34 +0800

    Update ProductList.aspx.designer.cs

--- ArtMoments/Sites/artist/ProductList.aspx.designer.cs
+++ ArtMoments/Sites/artist/ProductList.aspx.designer.cs
@@ -15,13 +15,13 @@ namespace ArtMoments.Sites.artist
     {
 
         /// <summary>
-        /// Button2 control.
+        /// addNetworkBtn control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
+        protected global::System.Web.UI.WebControls.Button addNetworkBtn;
 
         /// <summary>
         /// txtSearch control.
