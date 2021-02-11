3bb28da391bd56409e0ceaf9855776662b964273
HueyPeng17 <59443995+HueyPeng17@users.noreply.github.com>
2021-02-11 10:43:56 +0800

    Update AddProduct.aspx.designer.cs

--- ArtMoments/Sites/artist/AddProduct.aspx.designer.cs
+++ ArtMoments/Sites/artist/AddProduct.aspx.designer.cs
@@ -60,13 +60,13 @@ namespace ArtMoments.Sites.artist
         protected global::System.Web.UI.WebControls.SqlDataSource test;
 
         /// <summary>
-        /// fileUpload control.
+        /// ImageUpload control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.FileUpload fileUpload;
+        protected global::System.Web.UI.WebControls.FileUpload ImageUpload;
 
         /// <summary>
         /// txtArtworkPrice control.
