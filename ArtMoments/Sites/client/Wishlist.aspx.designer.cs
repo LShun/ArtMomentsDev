0352a8e2b0d450bce2305ec99ab49e5ed56d509b
YChuaC0621 <67249007+YChuaC0621@users.noreply.github.com>
2021-02-16 23:53:13 +0800

    UpdateWishlist

--- ArtMoments/Sites/client/Wishlist.aspx.designer.cs
+++ ArtMoments/Sites/client/Wishlist.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace ArtMoments.Sites.client
         protected global::System.Web.UI.WebControls.TextBox txtSearch;
 
         /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
         /// gvWishlist control.
         /// </summary>
         /// <remarks>
