c30a742b8fdfe6f55d19a7e554321cd1c01ec837
HueyPeng17 <59443995+HueyPeng17@users.noreply.github.com>
2021-02-17 10:47:47 +0800

    Update General.Master.designer.cs

--- ArtMoments/Masters/General.Master.designer.cs
+++ ArtMoments/Masters/General.Master.designer.cs
@@ -33,6 +33,15 @@ namespace ArtMoments.Masters
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// ArtworkMenu control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu ArtworkMenu;
+
+        /// <summary>
         /// smArtMoments control.
         /// </summary>
         /// <remarks>
