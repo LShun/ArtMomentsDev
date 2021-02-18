291c49925edc1550b2342d6cdfbd54ed4f807662
Suying <cheahsy-pm18@student.tarc.edu.my>
2021-02-18 17:16:17 +0800

    Update orderArt CSS & naming & comment

--- ArtMoments/Sites/general/OrderArt.aspx.cs
+++ ArtMoments/Sites/general/OrderArt.aspx.cs
@@ -36,13 +36,16 @@ namespace ArtMoments.Sites.general
         {
             if (!IsPostBack)
             {
+                Session["Username"] = 1;
+                Session["UserType"] = 1;
                 Session["ProdId"] = Request.QueryString["id"];
-                Session["ProdId"] = 3;
+                Session["ProdId"] = 8;
 
                 if (Session["ProdId"] != null)
                 {
                     using (SqlConnection con = new SqlConnection(conString))
                     {
+                        // Extract product information
                         using (SqlCommand cmd = new SqlCommand("select P.id as [prod-id], P.prod_name as [prod-name], P.prod_size [prod-size], P.prod_description as [prod-descrip], P.prod_image as [prod-image], P.prod_price as [prod-price] , P.prod_stock as [prod-stock], P.prod_sales as [prod-sales], C.category_name as [category-name], C.category_name as [category_name], U.user_name as [author], U.bibliography as [bibliography], U.profile_pic as [author_profilePic] from [Product] P left join [Product_Category] C on C.id = P.category_id left join [User] U on U.id = P.user_id where P.id like @ProdId"))
                         {
                             cmd.Parameters.AddWithValue("@ProdId", (String)Session["ProdId"].ToString());
@@ -54,10 +57,11 @@ namespace ArtMoments.Sites.general
                                 {
                                     sda.Fill(prodInfo);
 
+                                    // Display product infomation
                                     foreach (DataRow rw in prodInfo.Rows)
                                     {
                                         byte[] imgBinUrlProd = (byte[])prodInfo.Rows[0]["prod-image"];
-                                        artworkImage.Src = string.Format("data:image/png;base64,{0}", Convert.ToBase64String(imgBinUrlProd));
+                                        Imgartwork.Src = string.Format("data:image/png;base64,{0}", Convert.ToBase64String(imgBinUrlProd));
                                         lblartworkName.Text = prodInfo.Rows[0]["prod-name"].ToString();
                                         lblartworkID.Text = prodInfo.Rows[0]["prod-id"].ToString();
                                         lblartworkSize.Text = prodInfo.Rows[0]["prod-size"].ToString();
@@ -67,11 +71,10 @@ namespace ArtMoments.Sites.general
                                         lblartworkPrice.Text = ((Double)prodInfo.Rows[0]["prod-price"]).ToString("F");
                                         artworkPricePerPiece.Text = ((Double)prodInfo.Rows[0]["prod-price"]).ToString("F");
                                         Session["ProdPrice"] = prodInfo.Rows[0].Field<System.Double>("prod-price");
-                                        //artworkPrice = prodInfo.Rows[0].Field<System.Double>("prod-price");
                                         lblStock.Text = prodInfo.Rows[0]["prod-stock"].ToString();
                                         checkStock();
                                         byte[] imgBinUrlAuthor = (byte[])prodInfo.Rows[0]["author_profilePic"];
-                                        authorImage.Src = string.Format("data:image/png;base64,{0}", Convert.ToBase64String(imgBinUrlAuthor));
+                                        imgAuthorProfile.Src = string.Format("data:image/png;base64,{0}", Convert.ToBase64String(imgBinUrlAuthor));
                                         lblauthorInfoName.Text = prodInfo.Rows[0]["author"].ToString();
                                         lblauthorBibliography.Text = prodInfo.Rows[0]["bibliography"].ToString();
                                         Session["CurrentSales"] = prodInfo.Rows[0]["prod-sales"];
@@ -79,38 +82,38 @@ namespace ArtMoments.Sites.general
                                 }
                             }
                         }
-                        //check wishlist
+                        // Only allow buyer to access to buy & add to wishlist for the artwork
+                        // Buyer = 
                         if(Session["Username"] != null && Session["UserType"].Equals(1))
                         {
+                            // Enable all the button and textbox for buyer accessibility to buy
                             btnwishlistOff.Enabled = true;
                             btnwishlistOn.Enabled = true;
 
                             btnMinusQty.Enabled = true;
                             btnPlusQty.Enabled = true;
                             txtboxQty.Enabled = true;
-                            btnBuyNow.Enabled = true; 
+                            lbBuyNow.Enabled = true; 
 
+                            // checkwhether product is ald added in the wishlist
                             string wishlistQuery = "select W.id as [wishlist-id] from [Wishlist] W where W.user_id like @CustId and W.product_id like @ProdId";
                             using (SqlConnection conn = new SqlConnection(conString))
                             {
                                 SqlCommand cmd = new SqlCommand(wishlistQuery, conn);
                                 cmd.Parameters.Add("@CustId", (String)Session["Username"].ToString());
                                 cmd.Parameters.Add("@ProdId", (String)Session["ProdId"].ToString());
-                                try
-                                {
-                                    conn.Open();
 
-                                    if (cmd.ExecuteScalar() != null)
-                                    {
-                                        btnwishlistOn.Visible = true;
-                                        btnwishlistOff.Visible = false;
-                                    }
-                                }
-                                catch (Exception ex)
+                                conn.Open();
+
+                                if (cmd.ExecuteScalar() != null)
                                 {
+                                    // if in the wishlist, visible the on wishlist button, vice versa
+                                    btnwishlistOn.Visible = true;
+                                    btnwishlistOff.Visible = false;
                                 }
                             }
                         }
+                        // for non-buyer, diable all the access to buy & add art to wishlist
                         else
                         {
                             btnwishlistOn.Visible = false;
@@ -121,27 +124,27 @@ namespace ArtMoments.Sites.general
                             btnMinusQty.Enabled = false;
                             btnPlusQty.Enabled = false;
                             txtboxQty.Enabled = false;
-                            btnBuyNow.Enabled = false;
-                            btnBuyNow.Text = "Login As Buyer To Buy";
+                            lbBuyNow.Enabled = false;
+                            lbBuyNow.Text = "Login As Buyer To Buy";
                         }
                     }
                 }
             }
         }
 
+        // to check whether art is available for purchse
         protected void checkStock()
         {
             int availableStock = Convert.ToInt32(lblStock.Text);
             if (availableStock == 0)
             {
-                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Found')", true);
-
                 txtboxQty.Text = "0";
                 txtboxQty.Enabled = false;
-                btnBuyNow.Enabled = false;
+                lbBuyNow.Enabled = false;
             }
         }
 
+        // to calculate the total price with different quantity
         public void calculatePrice(int qty)
         {
             string artworkPrice = Session["ProdPrice"].ToString();
@@ -162,6 +165,7 @@ namespace ArtMoments.Sites.general
             lblartworkPrice.Text = subtotal.ToString("0.00");
         }
 
+        // minus the qty when user click minus button and check for the current qty to not less than 1
         protected void btnMinus_Click(object sender, EventArgs e)
         {
             int custOrder = Convert.ToInt32(txtboxQty.Text);
@@ -173,53 +177,42 @@ namespace ArtMoments.Sites.general
             }
         }
 
-
+        // if the wishlist button is click from off to on, add the art to buyer wishlist
         protected void btnwishlistOff_Click(object sender, EventArgs e)
         {
             btnwishlistOn.Visible = true;
             btnwishlistOff.Visible = false;
+          
+            SqlConnection con = new SqlConnection(conString);
 
-            try
-            {
-                SqlConnection con = new SqlConnection(conString);
-
-                string addWishlistQuery = "insert into [Wishlist] (product_id, user_id) VALUES (@ProdId, @CustId)";
-                SqlCommand cmd = new SqlCommand(addWishlistQuery, con);
-                cmd.Parameters.AddWithValue("@CustId", Convert.ToInt32(Session["Username"]));
-                cmd.Parameters.AddWithValue("@ProdId", Convert.ToInt32(Session["ProdId"]));
-                con.Open();
-                cmd.ExecuteNonQuery();
-                con.Close();
-
-            }
-            catch (Exception ex)
-            {
-
-            }
+            string addWishlistQuery = "insert into [Wishlist] (product_id, user_id) VALUES (@ProdId, @CustId)";
+            SqlCommand cmd = new SqlCommand(addWishlistQuery, con);
+            cmd.Parameters.AddWithValue("@CustId", Convert.ToInt32(Session["Username"]));
+            cmd.Parameters.AddWithValue("@ProdId", Convert.ToInt32(Session["ProdId"]));
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
         }
 
+        // when the button is click from on to off, delete the art from buyer wishlist
         protected void btnwishlistOn_Click(object sender, EventArgs e)
         {
             btnwishlistOn.Visible = false;
             btnwishlistOff.Visible = true;
 
-            try
-            {
-                SqlConnection con = new SqlConnection(conString);
+            SqlConnection con = new SqlConnection(conString);
 
-                string removeWishlistQuery = "Delete from [Wishlist] where user_id like @CustId and product_id like @ProdId";
-                SqlCommand cmd = new SqlCommand(removeWishlistQuery, con);
-                cmd.Parameters.AddWithValue("@CustId", Convert.ToInt32(Session["Username"]));
-                cmd.Parameters.AddWithValue("@ProdId", Convert.ToInt32(Session["ProdId"]));
-                con.Open();
-                cmd.ExecuteNonQuery();
-                con.Close();
+            string removeWishlistQuery = "Delete from [Wishlist] where user_id like @CustId and product_id like @ProdId";
+            SqlCommand cmd = new SqlCommand(removeWishlistQuery, con);
+            cmd.Parameters.AddWithValue("@CustId", Convert.ToInt32(Session["Username"]));
+            cmd.Parameters.AddWithValue("@ProdId", Convert.ToInt32(Session["ProdId"]));
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
 
-            }
-            catch (Exception ex)
-            {
-            }
         }
+
+        // if the current art is already added by buyer previously, increae the art quantity which previously in the cart
         protected void updateCartItemss(int totalQty)
         {
             string updateCartItemssQuery = "update [CartItems] set [quantity_int] = @UpdateQty where user_id like @CustId and product_id like @ProdId";
@@ -236,30 +229,26 @@ namespace ArtMoments.Sites.general
             }
         }
 
+        // add the art to the cart (previously not in the cart)
         void addNewIntoCart()
         {
-            try
-            {
+           
+            SqlConnection conn = new SqlConnection(conString);
+            string addCartQuery = "insert into [CartItems] (product_id,quantity,delivery_id, user_id) VALUES (@ProdId, @Qty, @DeliveryId, @CustId)";
+            //string addOrderQuery = "insert into [Order] (product_id,transaction_id) VALUES (@ProdId, @TransacId)";
+            SqlCommand cmdOrder = new SqlCommand(addCartQuery, conn);
+            conn.Open();
+            cmdOrder.Parameters.AddWithValue("@ProdId", Convert.ToInt32(Session["ProdId"]));
+            cmdOrder.Parameters.AddWithValue("@Qty", Convert.ToInt32(txtboxQty.Text));
+            cmdOrder.Parameters.AddWithValue("@CustId", Convert.ToInt32(Session["Username"]));
+            cmdOrder.Parameters.AddWithValue("@DeliveryId", Convert.ToInt32(ddlDeliveryMethod.SelectedValue));
 
-                SqlConnection conn = new SqlConnection(conString);
-                string addCartQuery = "insert into [CartItems] (product_id,quantity,delivery_id, user_id) VALUES (@ProdId, @Qty, @DeliveryId, @CustId)";
-                //string addOrderQuery = "insert into [Order] (product_id,transaction_id) VALUES (@ProdId, @TransacId)";
-                SqlCommand cmdOrder = new SqlCommand(addCartQuery, conn);
-                conn.Open();
-                cmdOrder.Parameters.AddWithValue("@ProdId", Convert.ToInt32(Session["ProdId"]));
-                cmdOrder.Parameters.AddWithValue("@Qty", Convert.ToInt32(txtboxQty.Text));
-                cmdOrder.Parameters.AddWithValue("@CustId", Convert.ToInt32(Session["Username"]));
-                cmdOrder.Parameters.AddWithValue("@DeliveryId", Convert.ToInt32(ddlDeliveryMethod.SelectedValue));
+            cmdOrder.ExecuteNonQuery();
+            conn.Close();
 
-                cmdOrder.ExecuteNonQuery();
-                conn.Close();
-            }
-            catch (Exception ex)
-            {
-
-            }
         }
 
+        // check whether the art is ald in the cart
         protected void checkInCart()
         {
 
@@ -269,39 +258,30 @@ namespace ArtMoments.Sites.general
                 SqlCommand cmd = new SqlCommand(cartChkQuery, conn);
                 cmd.Parameters.Add("@CustId", (String)Session["Username"].ToString());
                 cmd.Parameters.Add("@ProdId", (String)Session["ProdId"].ToString());
-                try
-                {
-                    conn.Open();
 
-                    if (cmd.ExecuteScalar() != null)
-                    {
-                        // update
-                        int totalQty = Convert.ToInt32(txtboxQty.Text) + (Int32)cmd.ExecuteScalar();
-                        updateCartItemss(totalQty);
-                    }
-                    else
-                    {
-                        addNewIntoCart();
-                    }
-                }
-                catch (Exception ex)
+                conn.Open();
+
+                if (cmd.ExecuteScalar() != null)
                 {
+                    // update
+                    int totalQty = Convert.ToInt32(txtboxQty.Text) + (Int32)cmd.ExecuteScalar();
+                    updateCartItemss(totalQty);
                 }
+                else
+                {
+                    addNewIntoCart();
+                }
+
             }
         }
 
+        // action perform when user click the buy now button
         protected void btnBuyNow_Click(object sender, EventArgs e)
         {
-            try
-            {
-                checkInCart();
-            }
-            catch (Exception ex)
-            {
-
-            }
+            checkInCart();
         }
 
+        // action perform when user click the plus button, the item qty should not over the available qty
         protected void btnPlusQty_Click(object sender, EventArgs e)
         {
             int custOrder = Convert.ToInt32(txtboxQty.Text);
