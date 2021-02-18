d26d86d05529dd022c48e2b3e071265f9038350d
HueyPeng17 <59443995+HueyPeng17@users.noreply.github.com>
2021-02-18 08:22:48 +0800

    Create Client.Master.cs

---
+++ ArtMoments/Masters/Client.Master.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ArtMoments.Masters
+{
+    public partial class Client : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string path = Request.AppRelativeCurrentExecutionFilePath;
+            foreach (MenuItem item in ArtworkMenu.Items)
+            {
+                item.Selected = item.NavigateUrl.Equals(path, StringComparison.InvariantCultureIgnoreCase);
+            }
+        }
+
+    }
+}
