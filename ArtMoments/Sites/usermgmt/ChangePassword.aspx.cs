c5e9f681a89da0d9b44bfca5c27090a86eaedef7
jia-ern <67193913+jia-ern@users.noreply.github.com>
2021-02-18 16:03:12 +0800

    Update ChangePassword.aspx.cs

--- ArtMoments/Sites/usermgmt/ChangePassword.aspx.cs
+++ ArtMoments/Sites/usermgmt/ChangePassword.aspx.cs
@@ -6,22 +6,19 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Data;
+using System.Configuration;
 
 
 namespace ArtMoments.Sites.usermgmt
 {
     public partial class ChangePassword : System.Web.UI.Page
     {
-        string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ArtMomentsDb;Integrated Security=True";
+        //string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ArtMomentsDb;Integrated Security=True";
+        string connectionString = ConfigurationManager.ConnectionStrings["ArtMomentsDbConnectionString"].ConnectionString;
 
         protected void Page_PreInit(object sender, EventArgs e)
         {
-            if (Session["Username"] == null)
-            {
-                MasterPageFile = "~/Masters/General.Master";
-
-            }
-            else if (Session["UserType"].ToString().Equals("2"))
+            if (Session["UserType"].ToString().Equals("2"))
             {
                 MasterPageFile = "~/Masters/Artist.Master";
             }
