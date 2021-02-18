885010268b51d5c51c30bdd2e400614e792e92ff
HueyPeng17 <59443995+HueyPeng17@users.noreply.github.com>
2021-02-18 08:22:36 +0800

    Create Artist.Master.cs

---
+++ ArtMoments/Masters/Artist.Master.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ArtMoments.Masters
+{
+    public partial class Artist : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string path = Request.AppRelativeCurrentExecutionFilePath;
+            foreach (MenuItem item in ArtworkMenu.Items)
+            {
+                item.Selected = item.NavigateUrl.Equals(path, StringComparison.InvariantCultureIgnoreCase);
+            }
+        }
+    }
+}
