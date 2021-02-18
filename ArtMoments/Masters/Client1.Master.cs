4e98206f284ad55f4085487b36164b84bfcf78fe
HueyPeng17 <59443995+HueyPeng17@users.noreply.github.com>
2021-02-18 13:31:24 +0800

    Create Client1.Master.cs

---
+++ ArtMoments/Masters/Client1.Master.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ArtMoments.Masters
+{
+    public partial class Client1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string path = Request.AppRelativeCurrentExecutionFilePath;
+            foreach (MenuItem item in ArtworkMenu.Items)
+            {
+                item.Selected = item.NavigateUrl.Equals(path, StringComparison.InvariantCultureIgnoreCase);
+            }
+        }
+        protected void ArtworkMenu_MenuItemClick(object sender, MenuEventArgs e)
+        {
+            if ((sender as Menu).SelectedItem.Text.Equals("Log Out"))
+            {
+                Session.RemoveAll();
+                Response.Redirect("../general/HomePage.aspx");
+            }
+
+        }
+    }
+}
