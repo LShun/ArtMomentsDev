4e8a563da04a1ddab2bc6d840c6e00a4fadfdcc1
HueyPeng17 <59443995+HueyPeng17@users.noreply.github.com>
2021-02-18 13:47:14 +0800

    Update BuyerSettingExtra.aspx.cs

--- ArtMoments/Sites/usermgmt/BuyerSettingExtra.aspx.cs
+++ ArtMoments/Sites/usermgmt/BuyerSettingExtra.aspx.cs
@@ -12,23 +12,6 @@ namespace ArtMoments.Sites.usermgmt
 
         protected void Page_PreInit(object sender, EventArgs e)
         {
-            if (Session["Username"] == null)
-            {
-                MasterPageFile = "~/Masters/General.Master";
-
-            }
-            else if (Session["UserType"].ToString().Equals("2"))
-            {
-                MasterPageFile = "~/Masters/Artist.Master";
-            }
-            else if (Session["UserType"].ToString().Equals("1"))
-            {
-                MasterPageFile = "~/Masters/Client.Master";
-            }
-        }
-
-        protected void Page_Load(object sender, EventArgs e)
-        {
             if (Session["UserType"].ToString().Equals("2"))
             {
                 MasterPageFile = "~/Masters/Artist1.Master";
@@ -37,6 +20,10 @@ namespace ArtMoments.Sites.usermgmt
             {
                 MasterPageFile = "~/Masters/Client1.Master";
             }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {          
 
         }
 
