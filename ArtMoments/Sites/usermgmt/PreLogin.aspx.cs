2889b3e685edbb31e78bb0a26ae82c13919e6551
jia-ern <67193913+jia-ern@users.noreply.github.com>
2021-02-19 16:04:39 +0800

    Update PreLogin.aspx.cs

--- ArtMoments/Sites/usermgmt/PreLogin.aspx.cs
+++ ArtMoments/Sites/usermgmt/PreLogin.aspx.cs
@@ -9,17 +9,6 @@ namespace ArtMoments.Sites.usermgmt
 {
     public partial class PreLogin : System.Web.UI.Page
     {
-        protected void Page_PreInit(object sender, EventArgs e)
-        {
-            if (Session["UserType"].ToString().Equals("2"))
-            {
-                MasterPageFile = "~/Masters/Artist1.Master";
-            }
-            else if (Session["UserType"].ToString().Equals("1"))
-            {
-                MasterPageFile = "~/Masters/Client1.Master";
-            }
-        }
 
         protected void Page_Load(object sender, EventArgs e)
         {
