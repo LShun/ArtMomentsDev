340746cd9fb60057129dcfca2146e487fb234ba7
HueyPeng17 <59443995+HueyPeng17@users.noreply.github.com>
2021-02-20 15:27:16 +0800

    Update OrderList.aspx.designer.cs

--- ArtMoments/Sites/artist/OrderList.aspx.designer.cs
+++ ArtMoments/Sites/artist/OrderList.aspx.designer.cs
@@ -31,5 +31,14 @@ namespace ArtMoments.Sites.artist
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView orderList;
+
+        /// <summary>
+        /// dsOrderList control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource dsOrderList;
     }
 }
