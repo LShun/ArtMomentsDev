9c7a654a9301c65298663de25158db6cd1c0bb10
jia-ern <67193913+jia-ern@users.noreply.github.com>
2021-03-31 16:58:18 +0800

    Add temp payment for email purpose

---
+++ ArtMoments/Sites/client/PaymentTemp.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PaymentTemp.aspx.cs" Inherits="ArtMoments.Sites.client.PaymentTemp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnPaid" runat="server" Text="Pay" OnClick="btnPaid_Click" />
+            <br />
+            <asp:Label ID="lblMsg" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ArtMoments/Sites/client/PaymentTemp.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ArtMoments.Sites.client
+{
+    public partial class PaymentTemp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnPaid_Click(object sender, EventArgs e)
+        {
+                try
+                {
+                    //SmtpClient smtp = new SmtpClient("127.0.0.1");
+                    SmtpClient client = new SmtpClient("Smtp.gmail.com", 587);  //74.125.24.108
+                    client.EnableSsl = true;
+                    client.Timeout = 10000;
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.UseDefaultCredentials = false;
+                    client.Credentials = new NetworkCredential("foojiaern2@gmail.com", "Jefoo1352");
+
+                    MailMessage message = new MailMessage();
+                    message.To.Add("foojiaern1@gmail.com");
+                    message.From = new MailAddress("foojiaern2@gmail.com");
+                    message.Subject = "Your Art Moments E-Receipt";
+                    message.Body = "Good day, " + "Thank you for shopping with us.\n" + "";
+                    client.Send(message);
+                    lblMsg.Text = "Email sent successfully!";
+                }
+                catch (SmtpException ex)
+                {
+                    lblMsg.Text = "Email failed to send!";
+                    Console.WriteLine(ex.ToString());
+                }
+        }        
+    }
+}
