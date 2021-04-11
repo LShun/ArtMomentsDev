18e5d4672179361621bb7354a3eaf599440b0d72
equinox.bot <9652305+LShun@users.noreply.github.com>
2021-04-11 15:12:06 +0800

    Add LoginStatus User Control

---
+++ ArtMoments/Content/user-controls/LoginStatus.ascx
@@ -0,0 +1,17 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="LoginStatus.ascx.cs" Inherits="ArtMoments.Content.user_controls.Labels" %>
+
+<table style="width: 100%;">
+    <tr>
+        <td>
+            <asp:Label ID="lblUsername" runat="server"></asp:Label>
+        </td>
+        <td>
+            <asp:Label ID="lblUserType" runat="server"></asp:Label>
+        </td>
+        <td>
+            <asp:Label ID="lblLoginTime" runat="server"></asp:Label>
+        </td>
+    </tr>
+</table>
+
+
---
+++ ArtMoments/Content/user-controls/LoginStatus.ascx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ArtMoments.Content.user_controls
+{
+    public partial class Labels : System.Web.UI.UserControl
+    {
+        public string Username { get; set; } = "";
+        public string UserType { get; set; } = "";
+        public string LoginTime { get; set; } = "";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblUsername.Text = "Welcome: " + "<b>" + Username + "<b>";
+            lblUserType.Text = "Type: " + "<b>" + UserType + "<b>";
+            lblLoginTime.Text = "Login Time: " + "<b>" + LoginTime + "<b>";
+        }
+    }
+}
