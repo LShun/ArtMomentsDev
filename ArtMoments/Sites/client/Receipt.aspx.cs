e7707f030d6a283d22b506a2d1c94042b9047fa3
HueyPeng17 <59443995+HueyPeng17@users.noreply.github.com>
2021-04-13 18:33:41 +0800

    Update Receipt.aspx.cs

--- ArtMoments/Sites/client/Receipt.aspx.cs
+++ ArtMoments/Sites/client/Receipt.aspx.cs
@@ -18,14 +18,15 @@ namespace ArtMoments.Sites.client
         {
             using (SqlConnection con = new SqlConnection(connectionString))
             {
-                using (SqlCommand cmd = new SqlCommand("SELECT top 1 id, date_order, delivery_fees, recv_name, recv_address FROM [Transaction] WHERE user_id" + Session["UserId"].ToString() + "order by id desc"))
+                using (SqlCommand cmd = new SqlCommand("SELECT top 1 id, date_order, delivery_fees, recv_name, recv_address FROM [Transaction] WHERE user_id = " + Session["UserId"].ToString() + "order by id desc"))
+                //using (SqlCommand cmd = new SqlCommand("SELECT top 1 id, date_order, delivery_fees, recv_name, recv_address FROM [Transaction] WHERE user_id = 3 order by id desc"))
                 {
                     cmd.CommandType = CommandType.Text;
                     cmd.Connection = con;
                     con.Open();
                     using (SqlDataReader sdr = cmd.ExecuteReader())
                     {
-                       
+
                         sdr.Read();
                         lblOrderDate.Text = sdr["date_order"].ToString();
                         lblName.Text = sdr["recv_name"].ToString();
@@ -37,22 +38,27 @@ namespace ArtMoments.Sites.client
                     con.Close();
                 }
 
-                //using (SqlCommand cmd = new SqlCommand("SELECT top 1 id, date_order, delivery_fees FROM [Transaction] WHERE user_id" + Session["UserId"].ToString() + "order by id desc"))
-                //{
-                //    cmd.CommandType = CommandType.Text;
-                //    cmd.Connection = con;
-                //    con.Open();
-                //    using (SqlDataReader sdr = cmd.ExecuteReader())
-                //    {
 
-                //        sdr.Read();
-                //        lblOrderDate.Text = sdr["date_order"].ToString();
-                //        string transactionId = sdr["id"].ToString();
-                //    }
-
-                //    con.Close();
-                //}
             }
+
+            Double total = 0;
+            Double deliveryFee = 0;
+            foreach (GridViewRow row in itemList.Rows)
+            {
+                Label subtotal = (Label)row.FindControl("SubTotal");
+                total += Convert.ToDouble(subtotal.Text.ToString());
+            }
+
+            Label deliveryFeefromDB = (Label)delieryFeeDetailView.FindControl("delivery_fees");
+            deliveryFee = Convert.ToDouble(deliveryFeefromDB.Text.ToString());
+            total += deliveryFee;
+            lblTotal.Text = total.ToString("F");
+        }
+
+        protected void itemList_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+
+
         }
     }
 }
